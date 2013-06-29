c0fd23d9a752bcb114d9b38985c3fe4915dd8a3d
Tycho Lursen <tycholursen@gmail.com>
2013-06-29 12:12:36 +0200

    Make the life of Debian users more easy When running an official Debian kernel, you don't have to recompile it anymore. We can patch the kernel for you.

--- contrib/sasc-ng/dvbloopback/module/config_dvb_new.pl
+++ contrib/sasc-ng/dvbloopback/module/config_dvb_new.pl
@@ -4,6 +4,7 @@ exec perl -x -- "$0" "$@"
 #!perl -w
 
 $debug = 0;
+$FILE="dvbdev-orig.h";
 
 sub test_dvb_adapter(\@) {
   my($inc) = @_;
@@ -17,15 +18,27 @@ sub test_dvb_adapter(\@) {
   $cmd = "cd config-dvb && make $vars" . ($debug ? "" : "2>/dev/null 1>/dev/null");
   print "$cmd\n" if($debug);
 
-  system("ln -sf chkdvb-2.6.v4l.c config-dvb/chkdvb.c");
-  system("ln -sf ../dvbdev-3.7.x.h config-dvb/dvbdev.h");
-   print "Using canned header\n";
+if (-e $FILE) {
+      system("ln -sf chkdvb-2.6.v4l.c config-dvb/chkdvb.c");
+      system("ln -sf ../dvbdev-orig.h config-dvb/dvbdev.h");
+      print "Using original header\n";
+    if(system("$cmd") == 0) {
+    `echo "DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr);" >> dvbdevwrap.h`; 
+    `echo "#define wrap_dvb_reg_adapter(a, b, c) dvb_register_adapter(a, b, c, &dvblb_basedev->dev, adapter_nr)" >> dvbdevwrap.h`; 
+     system("ln -sf dvbdev-orig.h dvbdev.h"); 
+    return 0;
+  }
+} else {
+      system("ln -sf chkdvb-2.6.v4l.c config-dvb/chkdvb.c");
+      system("ln -sf ../dvbdev-3.7.x.h config-dvb/dvbdev.h");
+      print "Using canned header\n";
     if(system("$cmd") == 0) {
     `echo "DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr);" >> dvbdevwrap.h`; 
     `echo "#define wrap_dvb_reg_adapter(a, b, c) dvb_register_adapter(a, b, c, &dvblb_basedev->dev, adapter_nr)" >> dvbdevwrap.h`; 
      system("ln -sf dvbdev-3.7.x.h dvbdev.h"); 
     return 0;
   }
+ }
 }
 
 exit(test_dvb_adapter(@ARGV));
