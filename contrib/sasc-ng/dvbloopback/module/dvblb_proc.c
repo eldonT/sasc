e3a04ace2e5759173304cbeffb2f4488e1b4385a
Tycho Lursen <tycholursen@gmail.com>
2013-07-17 12:06:59 +0200

    Fix compiling with 3.10 kernel - Part 1

--- contrib/sasc-ng/dvbloopback/module/dvblb_proc.c
+++ contrib/sasc-ng/dvbloopback/module/dvblb_proc.c
@@ -28,73 +28,9 @@
 
 #include <linux/proc_fs.h>
 #include "dvblb_internal.h"
+#include "internal.h"
 
 static struct proc_dir_entry *procdir;
-
-static int dvblb_procfs_read(char *page, char **start, off_t off, int count,
-                             int *eof, void *data)
-{
-	struct dvblb_devinfo *lbdev = (struct dvblb_devinfo *)data;
-	int val;
-	if (lbdev == NULL)
-		return 0;
-	val = (lbdev->forward_dev) ? 1 : 0;
-	return sprintf(page, "%03d", val);
-}
-
-static int dvblb_procfs_write(struct file *file, const char *buffer,
-                              unsigned long count, void *data)
-{
-	char str[10];
-	int val, v1, v2, v3, fm;
-	struct dvblb_devinfo *lbdev = (struct dvblb_devinfo *)data;
-	if (lbdev == NULL)
-		return count;
-	if (lbdev->parent->link == -1)
-		return count;
-	if (count > 10)
-		count = 10;
-	if (copy_from_user(str, buffer, count)) {
-		return -EFAULT;
-	}
-	val = simple_strtoul(str, NULL, 0);
-	v1 = val /100;
-	v2 = (val - v1*100) / 10;
-	v3 = val - v1*100 - v2*10;
-	if (v1 < 0 || v1 > 2) 
-		return -EFAULT;
-	if((fm = inuse_filemap(lbdev))) {
-		int type = lbdev->lb_dev->type;
-		printk("dvbloopback: Can't change forward on adapter%d."
-		       " Device %s still has %d users!\n",
-		       lbdev->parent->adapter.num, dnames[type], fm);
-		return count;
-	}
-	if (v3 == 1) {
-		struct list_head *entry;
-		list_for_each (entry, lbdev->parent->adapter_ll) {
-			struct dvb_adapter *adap;
-			adap = list_entry (entry, struct dvb_adapter,
-			                   list_head);
-			if (adap->num == lbdev->parent->link) {
-				struct list_head *entry0;
-				list_for_each (entry0,
-				               &adap->device_list) {
-					struct dvb_device *dev;
-					dev = list_entry (entry0,
-					       struct dvb_device, list_head);
-					if (dev->type == lbdev->lb_dev->type) {
-						lbdev->forward_dev = dev;
-						return count;
-					}
-				}
-			}
-		}
-	} else if (v3 == 0) {
-		lbdev->forward_dev = NULL;
-	}
-	return count;
-}
 
 static int dvblb_procfs_adapter_read(char *page, char **start, off_t off,
                                      int count, int *eof, void *data)
@@ -166,12 +102,10 @@ EXPORT_SYMBOL(dvblb_remove_procfs);
 int dvblb_init_procfs_device(struct dvblb *dvblb, struct dvblb_devinfo *lbdev)
 {
 	int type = lbdev->lb_dev->type;
-	lbdev->procfile = create_proc_entry(dnames[type], 0644, dvblb->procdir);
+	lbdev->procfile = proc_create(dnames[type], 0644, dvblb->procdir);
 	if (lbdev->procfile == NULL)
 		return -ENOMEM;
 	lbdev->procfile->data = lbdev;
-	lbdev->procfile->read_proc = dvblb_procfs_read;
-	lbdev->procfile->write_proc = dvblb_procfs_write;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,31)
 	lbdev->procfile->owner = THIS_MODULE;
 #endif
@@ -189,14 +123,12 @@ int dvblb_init_procfs_adapter(struct dvblb *dvblb)
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,31)
 	dvblb->procdir->owner = THIS_MODULE;
 #endif
-	dvblb->procfile = create_proc_entry("adapter", 0644, dvblb->procdir);
+	dvblb->procfile = proc_create("adapter", 0644, dvblb->procdir);
 	if (dvblb->procfile == NULL) {
 		dvblb_remove_procfs(dvblb->procdir, procdir);
 		return -ENOMEM;
 	}
 	dvblb->procfile->data = dvblb;
-	dvblb->procfile->read_proc = dvblb_procfs_adapter_read;
-	dvblb->procfile->write_proc = dvblb_procfs_adapter_write;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,31)
 	dvblb->procfile->owner = THIS_MODULE;
 #endif
